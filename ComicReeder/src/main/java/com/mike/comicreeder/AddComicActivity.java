208a262301e7e51a78585e6d89db60f6f3ac3eeb
Michael Napoleon <michael.napoleon@gmail.com>
2013-10-19 14:57:05 -0400

    changed the add comic form some

--- ComicReeder/src/main/java/com/mike/comicreeder/AddComicActivity.java
+++ ComicReeder/src/main/java/com/mike/comicreeder/AddComicActivity.java
@@ -3,6 +3,8 @@ package com.mike.comicreeder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
@@ -10,72 +12,100 @@ import android.annotation.TargetApi;
 import android.os.Build;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.parse.ParseObject;
 
 public class AddComicActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_comic);
-        // Show the Up button in the action bar.
-        setupActionBar();
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_add_comic);
+    // Show the Up button in the action bar.
+    setupActionBar();
 
+    EditText comicEditText = (EditText) findViewById(R.id.comicName);
 
-    public void addComic(View view) {
+    TextWatcher textWatcher = new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-      EditText comicNameText = (EditText)findViewById(R.id.comicName);
-      EditText writerText = (EditText)findViewById(R.id.writer);
-      EditText issueNumberText = (EditText)findViewById(R.id.issueNum);
-      EditText publisherText = (EditText)findViewById(R.id.publisher);
+      }
 
-      ParseObject comic = new ParseObject("Comics");
-      comic.put("comicName", comicNameText.getText().toString());
-      comic.put("writer", writerText.getText().toString());
-      comic.put("issue", Integer.parseInt(issueNumberText.getText().toString()));
-      comic.put("publisher", publisherText.getText().toString());
-
-      comic.saveInBackground();
-
-      Intent intent = new Intent(this, ComicReederActivity.class);
-      startActivity(intent);
-    }
-
-    /**
-     * Set up the {@link android.app.ActionBar}, if the API is available.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void setupActionBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-          getActionBar().setDisplayHomeAsUpEnabled(true);
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+        TextView tv1 = (TextView)findViewById(R.id.comicFloatingLabel);
+        if (s.length() == 0) {
+          tv1.setText("");
         }
-    }
+        else if (s.length() == 1) {
+          tv1.setText("Comic");
+        }
+      }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.add_comic, menu);
+      @Override
+      public void afterTextChanged(Editable s) {
+
+      }
+    };
+
+    comicEditText.addTextChangedListener(textWatcher);
+  }
+
+
+  public void addComic(View view) {
+
+    EditText comicNameText = (EditText) findViewById(R.id.comicName);
+    EditText writerText = (EditText) findViewById(R.id.writer);
+    EditText issueNumberText = (EditText) findViewById(R.id.issueNum);
+    EditText publisherText = (EditText) findViewById(R.id.publisher);
+
+    ParseObject comic = new ParseObject("Comics");
+    comic.put("comicName", comicNameText.getText().toString());
+    comic.put("writer", writerText.getText().toString());
+    comic.put("issue", Integer.parseInt(issueNumberText.getText().toString()));
+    comic.put("publisher", publisherText.getText().toString());
+
+    comic.saveInBackground();
+
+    Intent intent = new Intent(this, ComicReederActivity.class);
+    startActivity(intent);
+  }
+
+  /**
+   * Set up the {@link android.app.ActionBar}, if the API is available.
+   */
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+  private void setupActionBar() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+      getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.add_comic, menu);
+    return true;
+  }
+
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        // This ID represents the Home or Up button. In the case of this
+        // activity, the Up button is shown. Use NavUtils to allow users
+        // to navigate up one level in the application structure. For
+        // more details, see the Navigation pattern on Android Design:
+        //
+        // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+        //
+        NavUtils.navigateUpFromSameTask(this);
         return true;
     }
-    
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // This ID represents the Home or Up button. In the case of this
-                // activity, the Up button is shown. Use NavUtils to allow users
-                // to navigate up one level in the application structure. For
-                // more details, see the Navigation pattern on Android Design:
-                //
-                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-                //
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    return super.onOptionsItemSelected(item);
+  }
 
 }
