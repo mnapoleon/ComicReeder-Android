f4f4ba7e841b42bc92118e2b190754e32830aae9
Michael Napoleon <michael.napoleon@gmail.com>
2013-10-26 23:03:49 -0400

    fixed compilation errors due to package structure

--- ComicReeder/src/main/java/com/mike/comicreeder/components/FloatingLabelEditText.java
+++ ComicReeder/src/main/java/com/mike/comicreeder/components/FloatingLabelEditText.java
@@ -2,11 +2,12 @@ package com.mike.comicreeder.components;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.support.v7.appcompat.R;
 import android.util.AttributeSet;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.mike.comicreeder.R;
 
 /**
  * Created by michaelnapoleon on 10/21/13.
@@ -15,13 +16,20 @@ public class FloatingLabelEditText extends LinearLayout {
 
     TextView label;
     EditText textField;
+    String hintText;
+    String floatingLabelText;
 
     public FloatingLabelEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.FloatingLabelEditText);
+        hintText = a.getString(R.styleable.FloatingLabelEditText_editTextHint);
+        floatingLabelText = a.getString(R.styleable.FloatingLabelEditText_floatingLabelText);
 
         this.setOrientation(VERTICAL);
+
+        this.textField.setHint(hintText);
+        this.label.setText(floatingLabelText);
     }
 
 
