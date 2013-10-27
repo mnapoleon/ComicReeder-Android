dd84bbd9e825d331c10544a902fd536b14c4deaa
Michael Napoleon <michael.napoleon@gmail.com>
2013-10-27 15:27:41 -0400

    more changes for floating label component.  Still incomplete though.

--- ComicReeder/src/main/java/com/mike/comicreeder/components/FloatingLabelEditText.java
+++ ComicReeder/src/main/java/com/mike/comicreeder/components/FloatingLabelEditText.java
@@ -2,7 +2,10 @@ package com.mike.comicreeder.components;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -28,8 +31,39 @@ public class FloatingLabelEditText extends LinearLayout {
 
         this.setOrientation(VERTICAL);
 
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.floating_label_edit_text, this, true);
+
+        label = (TextView) findViewById(R.id.floatingLabel);
+        textField = (EditText) findViewById(R.id.textField);
+        if (floatingLabelText != null) {
+            this.label.setText(floatingLabelText);
+        }
+
         this.textField.setHint(hintText);
-        this.label.setText(floatingLabelText);
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //TextView textView = (TextView) findViewById(R.id.floatingLabel);
+                if (s.length() == 0) {
+                    label.setText("");
+                } else if (s.length() == 1) {
+                    label.setText(hintText);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        this.textField.addTextChangedListener(textWatcher);
     }
 
 
